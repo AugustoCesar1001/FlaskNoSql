87551fe3ae9d4bbfdf95eb90eae17bc374a023f2
Augusto <augustoscesars@hotmail.com>
2021-02-18 14:35:53 -0300

    Teste na Aplicação

--- app/models/mongodb.py
+++ app/models/mongodb.py
@@ -3,8 +3,7 @@ from bson.objectid import ObjectId
 
 def mongo_created_user(username, hashed_password, name, email, cellphone):
     id = mongo.db.users.insert(
-                {'username': username, 'password': hashed_password, 'name': name, 'email' : email, 'cellphone': cellphone}
-    )
+                {'username': username, 'password': hashed_password, 'name': name, 'email' : email, 'cellphone': cellphone})
     return id
 
 
